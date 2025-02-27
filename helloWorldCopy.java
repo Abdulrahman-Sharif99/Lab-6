739b64eb092778cbaa14e009141ef9d49c0ece85
Abdulrahman-Sharif99 <ec24856@qmul.ac.uk>
2025-02-27 15:51:51 +0000

    Add helloWorldCopy.java

---
+++ helloWorldCopy.java
@@ -0,0 +1,18 @@
+/**
+* This is a Javadoc comment for the class <b>HelloWorld</b>
+*
+* @author <a href="mailto:ec24856@qmul.ac.uk">Abdulrahman Sharif</a>
+* @version 1.1, March 2025
+*/
+public class HelloWorld {
+    /**
+     * Method to Print <b>Hello World</b>
+     * <p> This method uses the <b>System.out.print()</b> method to print HelloWorld to the console.</p>
+     *
+     * @param args[] string array
+     *
+     */
+     public static void main(String[] args) {
+     	   System.out.println("Hello World");
+     }
+} 
