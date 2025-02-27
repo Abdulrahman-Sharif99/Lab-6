34a782e03f356a7a523f3eef722fdfa64986e4b1
Abdulrahman-Sharif99 <ec24856@qmul.ac.uk>
2025-02-27 16:59:26 +0000

    Add mainBranchMethod to HelloWorld.java in main branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -15,4 +15,8 @@ public class HelloWorld {
      public static void main(String[] args) {
      	   System.out.println("Hello from main branch");
      }
+     public void mainBranchMethod(){
+        System.out.println("This is a method from main branch");
+        System.out.println("Hello from the mergeBranch");
+     }
 } 
